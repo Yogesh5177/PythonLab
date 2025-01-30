5e018fc972703c080cf394e0460addbcab315e0a
Yogesh5177 <yogeshkumarsharma5177@gmail.com>
2025-01-30 09:09:08 +0530

    Create toPrintCountdowntillZero.py

---
+++ toPrintCountdowntillZero.py
@@ -0,0 +1,9 @@
+# Taking user input
+num = int(input("Enter a number to start the countdown: "))
+
+# While loop for countdown
+while num >= 0:
+    print(num)
+    num -= 1  # Decrease the number by 1 in each iteration
+
+print("Countdown finished!")
